22126a0bf099c97f0cb9804bd7b60fede39ba8f2
eweeks <eweeks@g.harvard.edu>
2016-03-29 20:04:25 -0400

    Playing around with nps map.. but click events not fully working..

--- public/javascripts/yellmap.js
+++ public/javascripts/yellmap.js
@@ -1,4 +1,51 @@
-var markers;
+var NPMap = {
+ center: {
+   lat: 44.47,
+   lng: -110.58
+ },
+ div: 'mapYell',
+ overlays: [{
+   events: [{
+   fn: function() {
+     var site = '{{Site_Name}}'
+     console.log(site);
+     $.post('/getSite', {site: site}, function(data){
+             upDateSite(data);
+             console.log(data);
+       });
+   },
+   type: 'click'
+ }],
+   styles: {
+      point: {
+        'marker-color': '#5e9fd5',
+        'marker-size': 'medium',
+      }
+    },
+   popup: {
+     title: '{{Site_Name}}'
+   },
+   type: 'csv',
+   url: '/data/Soundscapes.csv'
+ }],
+ zoom: 9
+};
+
+(function () {
+ var s = document.createElement('script');
+ s.src = 'http://www.nps.gov/lib/npmap.js/2.0.0/npmap-bootstrap.js';
+ document.body.appendChild(s);
+})();
+
+function upDateSite(data){
+  console.log(data);
+    $(".sitename").text(data.Site_Name)
+    $(".sitecode").text(data.Site_Code)
+    console.log("Response is "+data.Site_Name);
+};
+
+
+/*var markers;
 //load csv files for maps, csv includes both parks
 d3.csv("/data/Soundscapes.csv", function(loadedRows) {
   markers = loadedRows;
@@ -31,4 +78,4 @@ var yellMap = L.map('mapYell', {
 L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains:['mt0','mt1','mt2','mt3']
-}).addTo(yellMap);
+}).addTo(yellMap);*/
